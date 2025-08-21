02a328a85e262f8e4061c4a2ba20f15c1d47d89e
yunsu-shin <166797095+shin-yunsu@users.noreply.github.com>
2025-08-21 17:56:59 +0900

    chore: 루트에서 home으로 리다이렉트

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,13 +1,13 @@
-import { Button } from "@workspace/ui/components/button"
-import { Typography } from "@workspace/ui/components/typography"
+"use client";
+import { Button } from "@workspace/ui/components/button";
+import { Typography } from "@workspace/ui/components/typography";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function Page() {
-  return (
-    <div className="flex items-center justify-center min-h-svh">
-      <div className="flex flex-col items-center justify-center gap-4">
-        <Typography variant="h1">Hello World</Typography>
-        <Button size="sm">Button</Button>
-      </div>
-    </div>
-  )
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/");
+  }, []);
+  return <div className="flex items-center justify-center min-h-svh"></div>;
 }
