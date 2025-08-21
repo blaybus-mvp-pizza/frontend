2e98bd545013d3bf13e1a741a6bfa467f1a7d7a3
yunsu-shin <166797095+shin-yunsu@users.noreply.github.com>
2025-08-21 19:20:01 +0900

    chore: 홈으로 리다이렉트

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,13 +1,12 @@
 "use client";
-import { Button } from "@workspace/ui/components/button";
-import { Typography } from "@workspace/ui/components/typography";
+
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
 export default function Page() {
   const router = useRouter();
   useEffect(() => {
-    router.push("/");
+    router.push("/home");
   }, []);
   return <div className="flex items-center justify-center min-h-svh"></div>;
 }
