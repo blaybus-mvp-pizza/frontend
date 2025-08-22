2551c47d4c2cdfad7d9c69b8a8d7b0b1393d862d
정서영 <diyung530@gmail.com>
2025-08-23 00:23:52 +0900

    [backoffice]: 이미지 url config 허용

--- apps/backoffice/next.config.mjs
+++ apps/backoffice/next.config.mjs
@@ -1,6 +1,28 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   transpilePackages: ["@workspace/ui"],
-}
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "nafalmvp-products.s3.ap-northeast-2.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "nafalmvp-popup-stores.s3.ap-northeast-2.amazonaws.com",
+        port: "",
+        pathname: "/**",
+      },
+      {
+        protocol: "https",
+        hostname: "example.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
+};
 
-export default nextConfig
+export default nextConfig;
